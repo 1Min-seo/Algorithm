647ca9ace5a9963ec80173321f89399bfc337569
1Min-seo <readex11@naver.com>
2022-09-02 16:57:20 +0900

    today is 2022-09-02(1)

--- src/ECT_main_2/dfs_bfs_1.java
+++ src/ECT_main_2/dfs_bfs_1.java
@@ -4,50 +4,45 @@ package ECT_main_2;
 import java.util.Scanner;
 
 public class dfs_bfs_1 {
-
+	
 	public static int n;
 	public static int m;
-	public static int [][]graph=new int[1000][1000];
+	public static int graph[][];
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
+		
 		n=sc.nextInt();
 		m=sc.nextInt();
-		
 		sc.nextLine();
-		
 		for(int i=0; i<n; i++) {
 			String str=sc.next();
 			for(int j=0; j<m; j++) {
 				graph[i][j]=str.charAt(j)-'0';
 			}
 		}
-		
 		sc.close();
 		
 		int result=0;
-		
 		for(int i=0; i<n; i++) {
 			for(int j=0; j<m; j++) {
 				if(dfs(i,j)) result+=1;
 			}
 		}
-		
 		System.out.println(result);
 	}
 	
 	public static boolean dfs(int x, int y) {
-		if(x<0 || x>=n || y<0 || y>=m) return false;
+		if(x<=-1 || x>=n || y<=-1 || y>=m) return false;
 		if(graph[x][y]==0) {
-			graph[x][y]=1;
 			dfs(x-1,y);
 			dfs(x,y-1);
 			dfs(x+1,y);
 			dfs(x,y+1);
-			
 			return true;
 		}
 		return false;
 	}
+	
 }
 
 //특정 지점에서 상하좌우를 살핀다.

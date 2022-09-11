57a87ad59d5e476437449dd9bf91051e91749823
1Min-seo <readex11@naver.com>
2022-09-11 15:19:10 +0900

    baek 2667

--- src/baek_dfs/dfs_2667.java
+++ src/baek_dfs/dfs_2667.java
@@ -1,66 +1,65 @@
 package baek_dfs;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.Scanner;
 
-public class dfs_2667{
-	private static int dx[] = {0,0,1,-1};
-    private static int dy[] = {1,-1,0,0};
-    private static int[] aparts = new int[25*25];
-    private static int n;
-    private static int apartNum = 0; //아파트 단지 번호의 수
-    private static boolean[][] visited = new boolean[25][25]; //방문여부
-    private static int[][] map = new int[25][25]; //지도
+public class dfs_2667 {
+	
+	public static int n;
+	public static int map[][]=new int[25][25];
+	public static boolean visited[][]=new boolean[25][25];
+	public static int totalNum=0; // 총 단지 수
+	public static int []townNum=new int[25*25]; //각 단지당 아파트의 수
+	public static int dx[]= {-1,0,0,1};
+	public static int dy[]= {0,-1,1,0};
+	
+	
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		sc.nextLine();
+		map=new int[n][n];
+		visited=new boolean[n][n];
+		
+		for(int i=0; i<n; i++) {
+			String str=sc.next();
+			for(int j=0; j<n; j++) {
+				map[i][j]=str.charAt(j)-'0';
+			}
+		}
+		sc.close();
+		
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<n; j++) {
+				if(map[i][j]==1 && !visited[i][j]) {
+					totalNum++;
+					dfs(i,j);
+				}
+			}
+		}
+		System.out.println(totalNum);
+		Arrays.sort(townNum);
+		for(int i=0; i<townNum.length; i++) {
+			if(townNum[i]==0) {
+			}else {
+				System.out.println(townNum[i]);
+			}
+		}
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-
-        map = new int[n][n];
-        visited = new boolean[n][n];
-
-        //전체 사각형 입력 받기
-        for(int i=0; i<n; i++){
-            String input = sc.next();
-            for(int j=0; j<n; j++){
-                map[i][j] = input.charAt(j)-'0';
-            }
-        }
-
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                if(map[i][j] == 1 && !visited[i][j]){
-                    apartNum++;
-                    dfs(i,j);
-                }
-            }
-        }
-
-        Arrays.sort(aparts);
-        System.out.println(apartNum);
-
-        for(int i=0; i<aparts.length; i++){
-            if(aparts[i] == 0){
-            }else{               
-                System.out.println(aparts[i]);
-            }
-        }
-    }
-
-    private static void dfs(int x, int y) {
-        visited[x][y] = true;
-        aparts[apartNum]++;
-
-        for(int i=0; i<4; i++){
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-
-            if(nx >=0 && ny >=0 && nx < n && ny < n){
-                if(map[nx][ny] == 1 && !visited[nx][ny]){
-                    dfs(nx,ny);
-                }
-            }
-        }
-    }
-    
+	}
+	
+	public static void dfs(int x, int y) {
+		visited[x][y]=true;
+		townNum[totalNum]++;
+		for(int i=0; i<4; i++) {
+			int nx=x+dx[i];
+			int ny=y+dy[i];
+			
+			if(nx>=0 && nx<n && ny>=0 && ny<n) {
+				if(map[nx][ny]==1 && !visited[nx][ny])
+					dfs(nx,ny);
+			}
+		}
+	}
 
 }

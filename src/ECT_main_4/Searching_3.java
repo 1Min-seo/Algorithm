508a075c43a98dbd87d7801c15e521d3f2720ec9
1Min-seo <readex11@naver.com>
2022-10-05 00:37:44 +0900

    정렬된 배열에서 특정 수의 개수 구하기 (이진탐색 이용하기)

---
+++ src/ECT_main_4/Searching_3.java
@@ -0,0 +1,34 @@
+//정렬된 배열에서 특정 수의 개수 구하기 (이진탐색 이용하기)
+package ECT_main_4;
+import java.util.*;
+
+public class Searching_3 {
+	
+	public static int leftIndex(int []arr, int start, int end, int x) {
+		while(start<=end) {
+			int mid=(start+end)/2;
+			if(mid<x) {
+				end=mid-1;
+			}
+			else start=mid+1;
+		}
+	}
+	public static int rightIndex(int []arr, int start, int end, int x) {}
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+		
+		int n=sc.nextInt();
+		int x=sc.nextInt();
+		int []arr=new int[n];
+		
+		for(int i=0; i<n; i++) {
+			arr[i]=sc.nextInt();
+		}
+		
+		
+
+	}
+
+}

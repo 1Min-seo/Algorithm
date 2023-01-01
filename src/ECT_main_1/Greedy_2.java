6d0652af8b3418150f16861d11276ec1545526b2
1Min-seo <readex11@naver.com>
2023-01-01 21:48:53 +0900

    Greedy_2

--- src/ECT_main_1/Greedy_2.java
+++ src/ECT_main_1/Greedy_2.java
@@ -6,30 +6,27 @@ import java.util.Scanner;
 public class Greedy_2 {
 
 	public static void main(String[] args) {
+		//1. N과 K를 입력받는다.
+		//2. K는 2이상의 자연수 이므로 1을 뺀 것 보다 나누는 것이 숫자가 크게 줄어든다.
+		//3. N에서 K를 나눈 나머지의 값만큼 카운트를 센다.
+		//4. N을 K로 나눈다. 이 값은 다시 N이 된다.
+		//5. 이를 반복한다.
+		//6. N이 K보다 작을 때 반복문을 나오게 되는데 1이 되어야 하므로 1을 뺀 값을 카운트에 더한다.
+
+		
 		Scanner sc=new Scanner(System.in);
-		int n=sc.nextInt(); //배열 크기
-		int m=sc.nextInt(); //총 m번 더함
-		int k=sc.nextInt(); //최대 k번 더할 수 있음
-		Integer []arr=new Integer[n];
-		int result=0;
+		int N=sc.nextInt();
+		int K=sc.nextInt();
 		int cnt=0;
 		
-		for(int i=0; i<arr.length; i++) {
-			arr[i]=sc.nextInt();
+		while(true) {
+			if(N<K) break;
+			cnt+=N%K;
+			N/=K;
+			cnt+=1;
 		}
-		Arrays.sort(arr);
-		int first=arr[n-1];
-		int second=arr[n-2];
 		
-		//가장 큰 수 더하는 횟수
-		cnt=m/(k+1)*k;
-		cnt+=m%(k+1);
-		result=first*cnt;
-		
-		//두 번째로 큰 수 더하기
-		result+=(second*(m/(k+1))); //또는 m에서 count를 뺴도 됨.
-		
-		System.out.println(result);
-		
+		cnt+=N-1;
+		System.out.println(cnt);
 	}
 }

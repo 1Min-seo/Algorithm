075d62407f98a3089cdb3c6730ec97cf6ba89750
1Min-seo <readex11@naver.com>
2023-01-01 21:46:42 +0900

    Greedy_1

--- src/ECT_main_1/Greedy_1.java
+++ src/ECT_main_1/Greedy_1.java
@@ -4,18 +4,16 @@ import java.util.*;
 public class Greedy_1 {
 
 	public static void main(String[] args) {
-		int []coinTypes= {500,100,50,10};
 		Scanner sc=new Scanner(System.in);
-		
-		int coin=sc.nextInt();
+		int coin[]= {500,100,50,10};
 		int cnt=0;
 		
-		for(int i=0; i<coinTypes.length; i++) {
-			cnt+=coin/coinTypes[i];
-			coin%=coinTypes[i];
+		int N=sc.nextInt();
+		for(int i=0; i<coin.length; i++) {
+			cnt+=N/coin[i];
+			N%=coin[i];
 		}
 		System.out.println(cnt);
 		
 	}
-
-}
+} 

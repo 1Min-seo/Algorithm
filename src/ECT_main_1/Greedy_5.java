20363d1f91e2197cc04395aa0a9dbae2d0ed0954
1Min-seo <readex11@naver.com>
2023-01-03 21:37:08 +0900

    큰 수의 법칙

--- src/ECT_main_1/Greedy_5.java
+++ src/ECT_main_1/Greedy_5.java
@@ -1,25 +1,46 @@
-//곱하기 혹은 더하기
+//큰 수의 법칙
 package ECT_main_1;
 import java.util.*;
 
 public class Greedy_5 {
-
 	public static void main(String[] args) {
+		
+		//1. N, M, K 값을 입력
+		//2. 배열 값을 입력
+		//3. 배열을 큰 순으로 정렬
+		//4. 반복문 while로 cnt를 하나씩 증가시키면서 cnt가 M이 될 때 종료
+		//5. K만큼 for문을 돌리고 끝나면 두번 째 값을 하나 더하고 다시 첫 번째 값을 더하는 형태로
 		Scanner sc=new Scanner(System.in);
-		String s=sc.next();
-		int result=s.charAt(0)-'0';
-		for(int i=1; i<s.length(); i++) {
-			int number=s.charAt(i)-'0';
-			if(result==0 || result==1) {
-				result+=number;
+		
+		int N=sc.nextInt();
+		int M=sc.nextInt();
+		int K=sc.nextInt();
+		int []arr=new int[N];
+		for(int i=0; i<N; i++) {
+			arr[i]=sc.nextInt();
+		}
+		
+		Arrays.sort(arr);
+		
+		int first=arr[N-1];
+		int second=arr[N-2];
+		int cnt=0;
+		int result=0;
+		
+		while(true) {
+			if(cnt==M) break;
+			for(int i=0; i<K; i++) {
+				cnt+=1;
+				result+=first;
+				if(cnt==M) break;
+				
 			}
-			else {
-				result*=number;
-			}
+			if(cnt==M) break;
+			result+=second;
+			cnt+=1;
 		}
 		
 		System.out.println(result);
-
 	}
 
 }

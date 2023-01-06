bace6db0ce0aec828432f05e95ff111eb1885070
1Min-seo <readex11@naver.com>
2023-01-06 20:44:01 +0900

    문자열 재정렬

--- src/ECT_main_1/Implementation_4.java
+++ src/ECT_main_1/Implementation_4.java
@@ -1,34 +1,39 @@
 //문자열 재정렬
 package ECT_main_1;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
 public class Implementation_4 {
 
+
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
+		//1. 문자열을 입력 받는다.
+		//2. 한 글자씩 문자를 읽으며 해당 문자가 문자열이면 문자열 배열에 넣는다.
+		//3. 해당 문자가 숫자라면 숫자를 더한다.
+		//4. 문자열 배열을 정렬한다.
+		//5. 문자열 배열 뒤에 숫자열 배열을 이어서 출력한다.
+		
+		Scanner sc=new Scanner (System.in);
 		String str=sc.next();
 		ArrayList<Character> charList=new ArrayList<Character>();
-		int sum=0;
+		int value=0;
 		
 		for(int i=0; i<str.length(); i++) {
 			if(Character.isLetter(str.charAt(i))) {
 				charList.add(str.charAt(i));
 			}
 			else {
-				sum+=str.charAt(i)-'0';
+				value+=str.charAt(i)-'0';
 			}
 		}
+		
 		Collections.sort(charList);
 		
 		for(int i=0; i<charList.size(); i++) {
 			System.out.print(charList.get(i));
 		}
-		System.out.println(sum);
+		System.out.println(value);
 	}
-
 }
 
-//알페벳 대문자와 숫자가 섞인 문자열을 입력 받는다.
-//문자열이 입력 되었을 때 문자를 하나씩 확인한다.
-//알파벳은 따로 리스트에 저장한다.
-//숫자는 합한다.
-//리스트에 저장된 알파벳을 출력한 다음 숫자를 붙여 출력한다.
+

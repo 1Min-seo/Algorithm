b68a6b4d9ef88e79b3a83e12cb26c65db66a4a79
1Min-seo <readex11@naver.com>
2023-01-06 13:36:12 +0900

    상하좌우

--- src/ECT_main_1/Implementation_1.java
+++ src/ECT_main_1/Implementation_1.java
@@ -5,33 +5,35 @@ import java.util.Scanner;
 public class Implementation_1 {
 	
 	public static void main(String[] args) {
+		//1. n 값을 입력 받는다.
+		//2. 이동할 계획서를 입력 받는다.
+		//3. 동서남북의 방향을 지정하는 moveType 배열을 만든다.
+		//4. 계획서와 moveType 배열을 하나씩 비교하며 방향을 움직인다.
 		Scanner sc=new Scanner(System.in);
 		int n=sc.nextInt();
+		sc.nextLine();
 		String[] plans=sc.nextLine().split(" ");
-		
-		//처음 위치
+		//동북서남
+		int []dx= {0,-1,0,1};
+		int []dy= {1,0,-1,0};
+		char []moveTypes= {'R','U','L','D'};
 		int x=1;
 		int y=1;
 		
-		int []dx= {0,0,-1,1};
-		int []dy={-1,1,0,0};
-		char []mvType= {'L','R','U','D'};
-		
 		for(int i=0; i<plans.length; i++) {
+			int nx=0;
+			int ny=0;
 			char plan=plans[i].charAt(0);
-			int nx=-1;
-			int ny=-1;
-			for(int j=0; j<mvType.length; j++) {
-				if(plan==mvType[j]) {
+			for(int j=0; j<moveTypes.length; j++) {
+				if(plan==moveTypes[j]) {
 					nx=x+dx[j];
 					ny=y+dy[j];
 				}
 			}
-			if(nx<1 || ny<1 || nx>x || ny>n) continue;
+			if(nx<1 || ny<1 || nx>n || ny>n) continue;
 			x=nx;
 			y=ny;
 		}
-		
 		System.out.println(x+" "+y);
 	}
 	

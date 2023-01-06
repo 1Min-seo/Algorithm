10183c2b1df480563de37df07d3d1733337add29
1Min-seo <readex11@naver.com>
2023-01-06 14:16:23 +0900

    시각

--- src/ECT_main_1/Implementation_2.java
+++ src/ECT_main_1/Implementation_2.java
@@ -3,26 +3,26 @@ package ECT_main_1;
 import java.util.*;
 public class Implementation_2 {
 	
+	public static boolean check(int h, int m, int s) {
+		if(h%10==3 || m%10==3 || m/10==3 || s%10==3 || s/10==3)
+			return true;
+		return false;
+	}
+	
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		int n=sc.nextInt();
-		int result=0; 
 		
-		for(int h=0; h<=n; h++) {
-			for(int m=0; m<60; m++) {
-				for(int s=0; s<60; s++) {
-					if (check(h,m,s)) result+=1;
+		Scanner sc=new Scanner (System.in);
+		int h=sc.nextInt();
+		int cnt=0;
+		
+		for(int i=0; i<=h; i++) {
+			for(int j=0; j<=59; j++) {
+				for(int k=0; k<=59; k++) {
+					if(check(i,j,k)) cnt+=1;
 				}
 			}
 		}
+		System.out.println(cnt);
 		
-		System.out.println(result);
-	}
-	
-	public static boolean check(int h, int m, int s) {
-		if(h%10==3 || m/10==3 || m%10==3 || s/10==3 || s%10==3) {
-			return true;
-		}
-		return false;
 	}
 }
